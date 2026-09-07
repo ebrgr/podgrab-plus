26d818f938db0a36242c8e50ef3a6f1c7c4939cd
ebrgr <evertonberger@gmail.com>
2026-09-07 10:45:36 -0300

    first iteration od /stats PR 289 dont include max download per podcast

--- controllers/podcast.go
+++ controllers/podcast.go
@@ -650,6 +650,36 @@ func RemoveTagFromPodcast(c *gin.Context) {
 	}
 }
 
+func GetStats(c *gin.Context) {
+	var items []db.PodcastItem
+
+	if err := db.GetAllPodcastItems(&items); err != nil {
+		fmt.Println(err.Error())
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request"})
+	}
+
+	var podcasts []db.Podcast
+	if err := db.GetAllPodcasts(&podcasts, ""); err != nil {
+		fmt.Println(err.Error())
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request"})
+	}
+
+	downloadedCount := 0
+	listenedCount := 0
+	podcastCount := len(podcasts)
+	for _, i := range items {
+		if i.DownloadStatus == db.Downloaded {
+			downloadedCount++
+		}
+		if i.IsPlayed {
+			listenedCount++
+		}
+	}
+
+	c.JSON(200, gin.H{"downloaded": downloadedCount, "listened": listenedCount, "podcasts": podcastCount})
+
+}
+
 func UpdateSetting(c *gin.Context) {
 	var model SettingModel
 	err := c.ShouldBind(&model)
